4e2eb3fae08002cef19f5840abad97f0f365e73f
ecotoxxplorer <39545176+ecotoxxplorer@users.noreply.github.com>
2018-09-04 11:18:43 -0400

    Update local_env.sh

--- local_env.sh
+++ local_env.sh
@@ -1,2 +1,3 @@
 # You can export any environment variable required for Galaxy to run or for tools setup.
+# Please make sure that the following paths are the same on your system.
 export DRMAA_LIBRARY_PATH=/usr/lib/slurm-drmaa/lib/libdrmaa.so.1
